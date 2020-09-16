17ee165e15ab8a8ee9366ccebcb72584eb5983a3
동민 <kebi3477@naver.com>
2020-09-16 11:31:27 +0000

    Clear BACKJOON(11729/step11)

--- src/test.cpp
+++ src/test.cpp
@@ -1,24 +1,19 @@
 #include <stdio.h>
-int n, cnt=0;
-using namespace std;
-void star(int x, int y, int size) {
-	if((x/size) % 3 == 1 && (y/size) % 3 == 1) {
-		printf(" ");	
+#include <math.h>
+int n;
+void hanoi(int n, int from, int by, int to) {
+	if(n == 1) {
+		printf("%d %d\n", from, to);
 	} else {
-		if(size/3 == 0) {
-			printf("*");	
-		} else {
-			star(x, y, size/3);
-		}
+		hanoi(n-1, from, to, by);
+		printf("%d %d\n", from, to);
+		hanoi(n-1, by, from, to);
 	}
 }
+
 int main(void) {
 	scanf("%d", &n);
-	for(int i = 0; i < n; i++) {
-		for(int j = 0; j < n; j++) {
-			star(i, j, n);
-		}
-		printf("\n");
-	}
+	printf("%d\n", (int)pow(2,n)-1);
+	hanoi(n,1,2,3);
 	return 0;
 }
