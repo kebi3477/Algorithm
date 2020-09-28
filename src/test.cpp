bb9b7fb3f8cf21bec8b23a5f18f59dd30286c7e8
동민 <kebi3477@naver.com>
2020-09-28 14:53:07 +0000

    Studying to 9663 in backtracking

--- src/test.cpp
+++ src/test.cpp
@@ -1,47 +1,91 @@
 #include <stdio.h>
-#include <cmath>
-
 using namespace std;
-int chess[15];
+int MAX = 15;
+int chess[15][15];
 int n, cnt=0;
 
-bool isPossible(int c) {
-	for(int i = 1; i < c; i++) {
-		if(chess[i] == chess[c]) {
-			return false;
-		}
-		if(abs(chess[i] - chess[c]) == abs(i - c)) {
-			return false;
+void clearChess() {
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < n; j++) {
+			chess[i][j] = 0;
 		}
 	}
-	return true;
 }
 
-void dfs(int row) {
-	if(row == n) {
+void checkQueen(int x, int y, int count) {
+	bool flag = false;
+	printf("x : %d,y : %d, count : %d\n", x,y,count);
+	if(count == n) {
 		cnt++;
-	} else {
-		for(int i = 1; i <= n; i++) {
-			chess[row + 1] = i;
-			if(isPossible(row+1)) {
-				dfs(row+1);
-			} else {
-				chess[row+1] = 0;
+		return;
+	}
+	
+	for(int i = 0; i < n; i++) {
+		if(flag) break;
+		if(chess[x][i] || chess[i][y]) {
+			flag = true;
+		}
+		if(chess[x+i][y+i]) {
+			flag = true;
+		}
+		if(x - i > -1 && y - i > -1) {
+			if(chess[x-i][y-i]) {
+				flag = true;
+			}
+		}
+		if(x - i > -1) {
+			if(chess[x-i][y+i]) {
+				flag = true;
+			}
+		}
+		if(y - i > -1) {
+			if(chess[x+i][y-i]) {
+				flag = true;
 			}
 		}
 	}
-	chess[row] = 0;
+	
+	// if(count == 0) {
+	// 	chess[x][y] = 1;
+	// 	count++;
+	// 	checkQueen(0, 0, count);
+	// }
+	
+	if(!flag) {
+		//printf("y : %d, ", y);
+		chess[x][y] = 1;
+		count++;
+	}
+	if(x+1 < n) {
+		checkQueen(x+1, y, count);
+		//printf("x : %d, ", x);
+	} else if (y+1 < n) {
+		checkQueen(0, y+1, count);
+	}
+	
 }
 
 int main(void) {
 	scanf("%d", &n);
 	
-	for(int i = 1; i <= n; i++) {
-		chess[1] = i;
-		dfs(1);
+	//chess[1][1] = 1;
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < n; j++) {
+			clearChess();
+			checkQueen(i, j, 0);
+			//chess[i][j] = 0;
+		}
+	}
+	//checkQueen(0, 0, 0);
+	
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < n; j++) {
+			printf("%d ", chess[i][j]);
+		}
+		printf("\n");
 	}
 	
-	printf("%d", cnt);
+	printf("cnt : %d", cnt);
 	
 	return 0;
 }
